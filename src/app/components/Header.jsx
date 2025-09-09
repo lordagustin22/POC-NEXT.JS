d5e4c32cae6979e9e64b5b4d25717fca58fdd80f
lordagustin22 <niconecrofilo589@gmail.com>
2025-09-09 00:37:40 -0300

    Los componentes ui son de cliente

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,27 +1,29 @@
+"use client";
+
 import Link from "next/link";
 
 export default function Header() {
-	return (
-		<header className="border-b bg-white/70 backdrop-blur supports-[backdrop-filter]:bg-white/60">
-			<div className="mx-auto max-w-7xl px-4 py-4 flex items-center justify-between">
-				<span className="text-xl font-bold tracking-tight text-emerald-700">
-					POC SSR
-				</span>
-				<nav className="flex gap-6 text-sm">
-					<Link href="/" className="hover:text-emerald-600">
-						Home
-					</Link>
-					<Link href="products" className="hover:text-emerald-600">
-						Productos
-					</Link>
-					<Link href="#" className="hover:text-emerald-600">
-						Acerca
-					</Link>
-					<Link href="#" className="hover:text-emerald-600">
-						Contacto
-					</Link>
-				</nav>
-			</div>
-		</header>
-	);
+    return (
+        <header className="border-b bg-white/70 backdrop-blur supports-[backdrop-filter]:bg-white/60">
+            <div className="mx-auto max-w-7xl px-4 py-4 flex items-center justify-between">
+                <span className="text-xl font-bold tracking-tight text-emerald-700">
+                    POC SSR
+                </span>
+                <nav className="flex gap-6 text-sm">
+                    <Link href="/" className="hover:text-emerald-600">
+                        Home
+                    </Link>
+                    <Link href="/products" className="hover:text-emerald-600">
+                        Productos
+                    </Link>
+                    <Link href="#" className="hover:text-emerald-600">
+                        Acerca
+                    </Link>
+                    <Link href="#" className="hover:text-emerald-600">
+                        Contacto
+                    </Link>
+                </nav>
+            </div>
+        </header>
+    );
 }
